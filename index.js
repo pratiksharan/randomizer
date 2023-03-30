2ad97c5a47b82fd594ba4318373b816b124a7e12
Pratik Sharan <pratiksharan56@gmail.com>
2023-03-30 12:14:36 +0530

    donee

--- index.js
+++ index.js
@@ -48,7 +48,31 @@ function getRandomData(type) {
   return data[type][rn(data[type].length)];
 }
 
-const memes = ['https://i.redd.it/a0v87gwzoge61.jpg', 'https://i.redd.it/q29egav34ee61.jpg', 'https://i.redd.it/iij16swxjie61.jpg', 'https://i.redd.it/vek7dm2hrge61.jpg', 'https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-8.png', 'https://miro.medium.com/max/1000/0*Ua695vjzFHV6VNOX.png', 'https://pbs.twimg.com/media/EKkPagPXkAA__Qo.jpg', 'https://code-love.com/wp-content/uploads/2019/03/download.jpeg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Programmer-while-sleeping.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Evolution-of-Memory-Storage-1024x996.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Programming-Memes-Error-in-Code-896x1024.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Coding-Meme-Code-Comments-be-Like-925x1024.jpg', 'https://www.thecoderpedia.com/wp-content/uploads/2020/06/Internet-Explorer-Joke-915x1024.jpg'];
+const memes = ['https://i.redd.it/9gcfson14sqa1.jpg', 
+              'https://i.redd.it/rf4hw76c6rqa1.jpg', 
+              'https://i.redd.it/m8gosda7tuqa1.jpg', 
+              'https://i.redd.it/ab2plzr7ymqa1.jpg', 
+              'https://i.redd.it/nonlxpvvirqa1.jpg', 
+              'https://i.redd.it/7q9gqt2ie1p51.jpg', 
+              'https://i.redd.it/crl4hlqc7e991.jpg',
+              'https://i.redd.it/mslmdg4xhfg81.jpg', 
+              'https://i.redd.it/p0pwhlli0xi81.jpg', 
+              'https://i.redd.it/lf2lstpys5481.png', 
+              'https://i.redd.it/lf2lstpys5481.png', 
+              'https://i.redd.it/fv1b7vektyd91.jpg', 
+              'https://i.redd.it/yihqq07kcnb81.jpg', 
+              'https://i.redd.it/ipy3w6vxdjj81.png', 
+              'https://i.redd.it/8om8jdtx2qf81.jpg', 
+              'https://i.redd.it/r6kwtr2cf7z81.jpg', 
+              'https://i.redd.it/lkee38551cia1.jpg', 
+              'https://i.redd.it/sykmv13cthf91.jpg', 
+              'https://i.redd.it/hzasrd2v4i891.png', 
+              'https://i.redd.it/ul9emakorila1.jpg', 
+              'https://i.redd.it/uq33rroab63a1.jpg', 
+              'https://i.redd.it/fo7vapg8lao81.jpg', 
+              'https://i.redd.it/hrerav12jpqa1.jpg', 
+              'https://i.redd.it/utgz9hcdjnqa1.jpg', 
+              'https://i.redd.it/vr5q29vh0sqa1.jpg'];
 
 const jokes = ['Eight bytes walk into a bar.  The bartender asks, “Can I get you anything?” “Yeah,” reply the bytes.  “Make us a double.”', 'There are only 10 kinds of people in this world: those who know binary and those who don’t.', 'All programmers are playwrights, and all computers are lousy actors.', 'Have you heard about the new Cray super computer?  It’s so fast, it executes an infinite loop in 6 seconds.', 'The generation of random numbers is too important to be left to chance.', 'Debugging: Removing the needles from the haystack.', '“Debugging” is like being the detective in a crime drama where you are also the murderer.', 'There are two ways to write error-free programs; only the third one works.', 'The best thing about a Boolean is even if you are wrong, you are only off by a bit.'];
 
